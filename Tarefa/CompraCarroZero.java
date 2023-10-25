57d4d177bb5bcc36d572778fd12827bb36a2d1c4
Pedro Cunegundes <95534653+PedroL1122@users.noreply.github.com>
2023-10-25 10:49:32 -0300

    Update CompraCarroZero.java

--- Tarefa/CompraCarroZero.java
+++ Tarefa/CompraCarroZero.java
@@ -5,7 +5,7 @@ public class CompraCarroZero extends Fabrica {
     @Override
     Carro montaCarro(String comprado) {
         if ("A".equals(comprado)) {
-            return new HilluxCar("Motor de 120 Cavalos", "Prata", "Tanque");
+            return new HyundaiCar("Motor de 110 Cavalos", "Preto", "Tanque");
         } else {
             return null;
         }
